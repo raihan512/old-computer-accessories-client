f587f9792551549bee3a891b0702d43435387b3d
raihan512 <raihangazi1024@Gmail.com>
2022-11-27 07:25:22 +0600

    image upload feature added

--- src/pages/Category/Category.js
+++ src/pages/Category/Category.js
@@ -4,11 +4,16 @@ import { useLoaderData } from 'react-router-dom';
 const Category = () => {
 
     const category = useLoaderData()
-    const { productname, productPrice, purchasedYear, productDescription, productCondition, phone, location } = category
+    const { productImg, productname, productPrice, purchasedYear, productDescription, productCondition, phone, location } = category
 
     return (
         <div>
             <h3 className='text-3xl font-bold uppercase'>We have total {category.length} items in this category</h3>
+            <div>
+                <div>
+                    <img src="" alt="" />
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/SellerPages/AddProducts/AddProducts.js
+++ src/pages/SellerPages/AddProducts/AddProducts.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useForm } from "react-hook-form";
 import { useLoaderData, useNavigate } from 'react-router-dom';
 import "react-datepicker/dist/react-datepicker.css";
 import toast from 'react-hot-toast';
+import { AuthProvider } from '../../../Contexts/Authprovider/AuthContext';
 
 const AddProducts = () => {
+    const { user } = useContext(AuthProvider);
     const navigate = useNavigate();
     const productCategories = useLoaderData();
     const { register, handleSubmit, formState: { errors } } = useForm();
     const imageHostKey = process.env.REACT_APP_imgbb_key;
-    console.log(imageHostKey);
+
+    const currentTime = `${new Date().toDateString()} ${new Date().toLocaleTimeString()}`
+
 
     const handleAddProducts = data => {
         const image = data.productImg[0]
@@ -25,7 +29,8 @@ const AddProducts = () => {
                 if (imgData.success) {
                     const imgUrl = imgData.data.url;
                     data.productImg = imgUrl;
-                    console.log(data);
+                    data.postedTime = currentTime;
+                    data.sellerName = user.displayName;
                     fetch('http://localhost:5000/products', {
                         method: "POST",
                         headers: {
@@ -45,53 +50,79 @@ const AddProducts = () => {
     }
     return (
         <div className='mx-5'>
-            <div className='max-w-[600px] border border-accent rounded-md my-20 mx-auto'>
+            <div className='max-w-[1000px] border border-accent rounded-md my-20 mx-auto'>
                 <h3 className='text-3xl font-bold uppercase text-center my-10 text-accent'>Add your products</h3>
-                <form className='px-20 pb-10' onSubmit={handleSubmit(handleAddProducts)}>
+                <form className='px-20 pb-10 grid grid-cols-1 md:grid-cols-2 gap-5' onSubmit={handleSubmit(handleAddProducts)}>
                     {/* Product name */}
-                    <input type="text" {...register("productname", { required: true })} placeholder="Add Your Product Name" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
-                    {errors.productname && <span className='text-red-500'>Add Your product name</span>}
+                    <div>
+                        <input type="text" {...register("productname", { required: true })} placeholder="Add Your Product Name" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
+                        {errors.productname && <span className='text-red-500'>Add Your product name</span>}
+                    </div>
+                    {/* Product Original Price */}
+                    <div>
+                        <input type="number" {...register('originalPrice', { required: true })} placeholder="Your Product original Price" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
+                        {errors.originalPrice && <span className='text-red-500'>You must have to add your Buying price</span>}
+                    </div>
                     {/* Product Selling Price */}
-                    <input type="number" {...register('productPrice', { required: true })} placeholder="Your Product selling Price" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
-                    {errors.productPrice && <span className='text-red-500'>You must have to add your selling price</span>}
+                    <div>
+                        <input type="number" {...register('resalePrice', { required: true })} placeholder="Your Product selling Price" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
+                        {errors.resalePrice && <span className='text-red-500'>You must have to add your selling price</span>}
+                    </div>
                     {/* Product Condition */}
-                    <select {...register("productCondition", { required: true })} className='select select-accent w-full mt-3'>
-                        <option value="">Product Condition</option>
-                        <option value="excellent">Excellent</option>
-                        <option value="good">Good</option>
-                        <option value="fair">Fair</option>
-                    </select>
-                    {errors.productCondition && <span className='text-red-500'>You must have to add your product condition</span>}
+                    <div>
+                        <select {...register("productCondition", { required: true })} className='select select-accent w-full mt-3'>
+                            <option value="">Product Condition</option>
+                            <option value="excellent">Excellent</option>
+                            <option value="good">Good</option>
+                            <option value="fair">Fair</option>
+                        </select>
+                        {errors.productCondition && <span className='text-red-500'>You must have to add your product condition</span>}
+                    </div>
                     {/* Product Category */}
-                    <select {...register("productCategory", { required: true })} className='select select-accent w-full mt-3'>
-                        <option value="">Product Category</option>
-                        {
-                            productCategories.map(productCategory => <option
-                                className='text-black'
-                                key={productCategory._id}
-                                value={productCategory._id}>
-                                {productCategory.title}</option>)
-                        }
-                    </select>
-                    {errors.productCategory && <span className='text-red-500'>You must have to add a product category</span>}
+                    <div>
+                        <select {...register("productCategory", { required: true })} className='select select-accent w-full mt-3'>
+                            <option value="">Product Category</option>
+                            {
+                                productCategories.map(productCategory => <option
+                                    className='text-black'
+                                    key={productCategory._id}
+                                    value={productCategory._id}>
+                                    {productCategory.title}</option>)
+                            }
+                        </select>
+                        {errors.productCategory && <span className='text-red-500'>You must have to add a product category</span>}
+                    </div>
                     {/* Add Phone Number */}
-                    <input type="number" {...register('phone', { required: true, minLength: 11, maxLength: 11 })} placeholder="Add Your Phone Number" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
-                    {errors.phone && <span className='text-red-500'>You must have to add your phone number with 11 Digit</span>}
+                    <div>
+                        <input type="number" {...register('phone', { required: true, minLength: 11, maxLength: 11 })} placeholder="Add Your Phone Number" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
+                        {errors.phone && <span className='text-red-500'>You must have to add your phone number with 11 Digit</span>}
+                    </div>
                     {/* Add Location */}
-                    <input type="text" {...register('location', { required: true })} placeholder="Add Your Location" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
-                    {errors.location && <span className='text-red-500'>You must have to add a location</span>}
+                    <div>
+                        <input type="text" {...register('location', { required: true })} placeholder="Add Your Location" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
+                        {errors.location && <span className='text-red-500'>You must have to add a location</span>}
+                    </div>
                     {/* Add purchased year */}
-                    <input type='number' {...register('purchasedYear', { required: true, minLength: 4 })} placeholder="Which year did you bought this?" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
-                    {errors.purchasedYear && <span className='text-red-500'>You must have to add purchased year like: 2015</span>}
+                    <div>
+                        <input type='number' {...register('purchasedYear', { required: true, minLength: 4 })} placeholder="Which year did you bought this?" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
+                        {errors.purchasedYear && <span className='text-red-500'>You must have to add purchased year like: 2015</span>}
+                    </div>
+                    {/* Add years of use */}
+                    <div>
+                        <input type='number' {...register('usedFor', { required: true })} placeholder="How many years used" className="input input-bordered input-accent w-full text-lg font-semibold mt-3" />
+                        {errors.usedFor && <span className='text-red-500'>Add how many years you used it</span>}
+                    </div>
                     {/* Add Product Image */}
-                    <input type="file" {...register('productImg', { required: true })} className="file-input w-full border border-accent mt-3" />
-                    {errors.productImg && <span className='text-red-500'>You must have to add your product image</span>}
+                    <div>
+                        <input type="file" {...register('productImg', { required: true })} className="file-input w-full border border-accent mt-3" />
+                        {errors.productImg && <span className='text-red-500'>You must have to add your product image</span>}
+                    </div>
                     {/* Add Product description optional */}
                     <textarea  {...register('productDescription')}
                         placeholder="Describe about your products"
-                        className='border border-accent w-full h-20 p-2 mt-3'></textarea>
+                        className='md:col-span-2 border border-accent w-full h-20 p-2 mt-3'></textarea>
 
-                    <input type="submit" value="Submit" className='btn bg-error border-0 w-full hover:bg-success text-lg font-semibold mt-5' />
+                    <input type="submit" value="Submit" className='md:col-span-2 btn bg-error border-0 w-full hover:bg-success text-lg font-semibold mt-5' />
                 </form>
             </div>
         </div >
